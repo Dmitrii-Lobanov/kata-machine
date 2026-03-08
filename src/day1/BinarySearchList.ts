98836ab8457f7cad227dceeea3407f80c348955f
Dmitrii <dmitriilobanov3@gmail.com>
2026-03-08 10:42:28 -0500

    01: Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,22 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
 
+    do {
+        const mid = Math.floor(lo + (hi - lo)/ 2);
+        const value = haystack[mid];
+
+        if (value === needle) {
+            return true;
+        }
+
+        if (value > needle) {
+            hi = mid; 
+        } else {
+            lo = mid + 1;
+        }
+
+    } while (lo < hi);
+
+    return false;
 }
